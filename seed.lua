a91804a383b8568ddc9be644f98d6218472b63c6
Michael Strobel <mstrobel97@gmail.com>
2023-10-27 15:06:38 +0200

    👰 seed two admins

--- seed.lua
+++ seed.lua
@@ -2,18 +2,35 @@ local User = require "models.User"
 local config = require("lapis.config").get()
 local bcrypt = require "bcrypt"
 
-local admin = User:find { name = "admin" }
+local michi = User:find { name = "michi" }
 
-local password = bcrypt.digest(config.admin_password, config.bcrypt_rounds)
+local password = bcrypt.digest(config.michi_password, config.bcrypt_rounds)
 
-if not admin then
+if not michi then
   User:create {
     name = "admin",
     password = password,
     is_admin = 1,
   }
 else
-  admin:update {
+  michi:update {
+    password = password,
+    is_admin = 1,
+  }
+end
+
+local annabell = User:find { name = "annabell" }
+
+local password = bcrypt.digest(config.annabell_password, config.bcrypt_rounds)
+
+if not annabell then
+  User:create {
+    name = "admin",
+    password = password,
+    is_admin = 1,
+  }
+else
+  annabell:update {
     password = password,
     is_admin = 1,
   }
