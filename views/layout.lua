af6f5e150ec23561125c72ca2e5aa9dc6cbcc070
Michael Strobel <mstrobel97@gmail.com>
2023-10-30 15:29:19 +0100

    🚔 move navbar into layout

--- views/layout.lua
+++ views/layout.lua
@@ -1,7 +1,23 @@
 ---@diagnostic disable:undefined-global
 local Widget = require("lapis.html").Widget
 
-local Navbar = require "views.components.navbar"
+local sites = {
+  {
+    text = "Wunschliste",
+    name = "wishlist",
+    logged_in = false,
+  },
+  {
+    text = "Kalender",
+    name = "calendar",
+    logged_in = true,
+  },
+  -- {
+  --   text = "Blog",
+  --   name = "blog",
+  --   logged_in = true,
+  -- },
+}
 
 return Widget:extend(function(self)
   raw "<!DOCTYPE HTML>"
@@ -19,9 +35,37 @@ return Widget:extend(function(self)
     end)
 
     body(function()
-      widget(Navbar())
+      nav(function()
+        ul(function()
+          for _, site in ipairs(sites) do
+            if not site.logged_in or self.session.user then
+              li({ class = { active = self.route_name == site.name } }, function()
+                a({ href = self:url_for(site.name) }, site.text)
+              end)
+            end
+          end
+
+          if self.session.user then
+            li({ class = "auth" }, function()
+              form({ id = "logout-form", class = "hidden", method = "POST", action = self:url_for "logout" }, function()
+                input { type = "hidden", name = "csrf_token", value = self.csrf_token }
+              end)
+
+              a({ href = "#", id = "logout-button" }, "Logout")
+            end)
+          else
+            li({ class = { "auth", active = self.route_name == "login" } }, function()
+              a({ href = self:url_for "login" }, "Login")
+            end)
+          end
+        end)
+      end)
 
       main(function()
+        if self.flash then
+          div({ class = { self.flash.status, "flash" } }, self.flash.message)
+        end
+
         self:content_for "inner"
       end)
     end)
